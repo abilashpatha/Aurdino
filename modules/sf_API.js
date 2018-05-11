28d290a822906a2b609b67ba235d6e7fe92298ab
abilashpatha <30526752+abilashpatha@users.noreply.github.com>
2018-05-11 16:28:18 +0530

    svgg

--- modules/sf_API.js
+++ modules/sf_API.js
@@ -39,7 +39,7 @@ var fetchToDo = function(psid){
 	console.log("inside fetchToDo Method"+JSON.stringify(psid));
 	return new Promise(function(resolve, reject){
 		//connection.query({query: "SELECT Name, Amount FROM Opportunity where Id ='0067F000004YR3c'" }, function(err, res)
-		connection.postUrl({url:"/services/data/v41.0/sobjects/temperature__e",body:JSON.stringify(psid)}, function(err, res)	
+		connection.postUrl({url:"/services/data/v41.0/sobjects/temperature__e",body:psid}, function(err, res)	
 				{
 		    if(err)
 		    { console.error(err);
